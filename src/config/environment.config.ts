855ea0587281a04844f9d43a92b880f7a3fa7f02
Mai Bảo Huy <106733089+baohuyxxi@users.noreply.github.com>
2025-09-07 20:04:30 +0700

    repussh

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,4 +1,5 @@
 import { registerAs } from '@nestjs/config';
+
 export default registerAs('app', () => ({
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
   DATABASE_NAME: process.env.DATABASE_NAME || 'crmDB',
