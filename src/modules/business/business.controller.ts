85347aef407d70a614559a3f059a486553c648c2
Mai Bảo Huy <106733089+baohuyxxi@users.noreply.github.com>
2025-09-18 15:18:41 +0700

    fix USER_..

--- src/modules/business/business.controller.ts
+++ src/modules/business/business.controller.ts
@@ -1,40 +1,47 @@
-import { Body, Controller, Get, Param, Post, Req, UseGuards } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { BusinessService } from "./business.service";
-import { CreateBusinessDto } from "./dto/business.dto";
-import { Business } from "./schemas/business.schema";
-import { Permission } from "../auth/permissions.enum";
-import { Permissions } from "../auth/decorators/permissions.decorator";
-import { PermissionsGuard } from "../auth/guards/permissions.guard";
-import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { BusinessService } from './business.service';
+import { CreateBusinessDto } from './dto/business.dto';
+import { Business } from './schemas/business.schema';
+import { Permission } from '../auth/permissions.enum';
+import { Permissions } from '../auth/decorators/permissions.decorator';
+import { PermissionsGuard } from '../auth/guards/permissions.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import express from 'express';
-
 
 @ApiTags('Businesses')
 @Controller('businesses')
 export class BusinessController {
-    constructor(private readonly businessService: BusinessService) { }
-    // Add more endpoints as needed
+  constructor(private readonly businessService: BusinessService) {}
+  // Add more endpoints as needed
 
-    @Post("add")
-    @UseGuards(JwtAuthGuard, PermissionsGuard)
-    @Permissions(Permission.USER_CREATE_ANY)
-    async createBusiness(@Body() body: CreateBusinessDto): Promise<Business> {
-        return this.businessService.createBusiness(body);
-    }
+  @Post('add')
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions(Permission.BUSINESS_CREATE)
+  async createBusiness(@Body() body: CreateBusinessDto): Promise<Business> {
+    return this.businessService.createBusiness(body);
+  }
 
-    @Get(":id")
-    @UseGuards(JwtAuthGuard, PermissionsGuard)
-    @Permissions(Permission.USER_READ_ANY)
-    async findOneBybusId(@Param('id') busId: string): Promise<Business | null> {
-        return this.businessService.findOneBybusId(busId);
-    }
+  @Get(':id')
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions(Permission.BUSINESS_READ_OWN, Permission.BUSINESS_READ_ANY)
+  async findOneBybusId(@Param('id') busId: string): Promise<Business | null> {
+    return this.businessService.findOneBybusId(busId);
+  }
 
-    @Get()
-    @UseGuards(JwtAuthGuard, PermissionsGuard)
-    @Permissions(Permission.USER_READ_ANY)
-    async findAllOfOwner(@Req() req: express.Request): Promise<Business[]> {
-        const user = req.user as { userId: string; roles: string[] };
-        return this.businessService.findAllOfOwner(user.userId);
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions(Permission.BUSINESS_READ_OWN, Permission.BUSINESS_READ_ANY)
+  async findAllOfOwner(@Req() req: express.Request): Promise<Business[]> {
+    const user = req.user as { userId: string; roles: string[] };
+    return this.businessService.findAllOfOwner(user.userId);
+  }
 }
